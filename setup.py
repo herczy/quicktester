4667acf3f5993e0f21974b168fb4e9b0ff714317
Viktor Hercinger <herczy@balabit.hu>
2014-10-07 23:42:35 +0200

    fixed problems with installing from an sdist

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 import re
 import sys
 try:
@@ -16,7 +16,7 @@ finally:
     del sys.path[0]
 
 
-with open('requirements.txt') as f:
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
     requires = [re.sub(r'^([^<>=]*)[<>=]=.*$', r'\1', line.strip()) for line in f if line]
 
 
