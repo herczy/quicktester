fcbe6389f613364e847a8796cdb90a3a30f4daaa
Hercinger Viktor <hercinger.viktor@balabit.com>
2014-10-15 20:09:25 +0200

    fnmap: added simple function name mapping

---
+++ quicktester/fnmap.py
@@ -0,0 +1,49 @@
+import collections
+import re
+import os.path
+
+
+class FilenameMapping(object):
+    def __init__(self, mappings):
+        self.__mappings = collections.OrderedDict(mappings)
+
+    def map(self, filename, variables=()):
+        variables = dict(variables)
+        filename = os.path.normpath(filename)
+
+        for pattern, map_to in self.__mappings.items():
+            pattern_replaced = self.__replace_variables(pattern, variables)
+
+            if re.match(pattern_replaced, filename):
+                filename = re.sub(
+                    pattern_replaced,
+                    self.__replace_variables(map_to, variables),
+                    filename
+                )
+                break
+
+        return os.path.normpath(filename)
+
+    def __replace_variables(self, pattern, variables):
+        res = pattern
+        for variable, value in variables.items():
+            res = res.replace('@{}@'.format(variable), value)
+
+        return res
+
+
+builtin_mappings = {
+    'default': FilenameMapping(
+        [
+            (r'(.*/test_.*\.py)', r'\1'),
+            (r'(.*)/.*\.py', r'\1'),
+        ]
+    ),
+
+    'external': FilenameMapping(
+        [
+            (r'(@BASEPATH@/@TESTDIR@(?:.*/test_.*\.py))', r'\1'),
+            (r'@BASEPATH@((?:/[^/]+)*/)(.*\.py)', r'@BASEPATH@/@TESTDIR@\1test_\2'),
+        ]
+    )
+}
---
+++ quicktester/tests/test_fnmap.py
@@ -0,0 +1,74 @@
+import unittest
+
+from ..fnmap import FilenameMapping, builtin_mappings
+
+
+class TestFilenameMapping(unittest.TestCase):
+    def setUp(self):
+        self.mapping = FilenameMapping(
+            [
+                (r'(.*/test_.*\.py)', r'\1'),
+                (r'(.*)/.*\.py', r'\1'),
+            ]
+        )
+
+    def test_map(self):
+        self.assertEqual('/path/to', self.mapping.map('/path/to/module.py'))
+
+    def test_map_maps_to_same_file(self):
+        self.assertEqual('/path/to/nonpython', self.mapping.map('/path/to/nonpython'))
+
+    def test_precedence_is_kept(self):
+        path = '/path/to/package/tests/test_something.py'
+
+        self.assertEqual(path, self.mapping.map(path))
+
+    def test_replace_variables_in_source(self):
+        workdir = '/path/to/workdir'
+        path = workdir + '/test.py'
+        altmapping = FilenameMapping([(r'@WORKDIR@/(.*\.py)', r'\1'),])
+
+        self.assertEqual(path, altmapping.map(path, {'WORKDIR': '/somedir'}))
+        self.assertEqual('test.py', altmapping.map(path, {'WORKDIR': workdir}))
+
+    def test_replace_variables_in_target(self):
+        altmapping = FilenameMapping([(r'.*\.py', r'@WORKDIR@'),])
+
+        self.assertEqual('workdir', altmapping.map('test_something.py', {'WORKDIR': 'workdir'}))
+
+    def test_normalize_argument_paths(self):
+        self.assertEqual('/path/to', self.mapping.map('///path/./to/some/../module.py'))
+
+    def test_normalize_result_paths(self):
+        self.assertEqual(
+            '/path/to/module.py',
+            FilenameMapping([(r'(.*)', r'///path/./to/some/../module.py')]).map('')
+        )
+
+
+class TestBuiltinMappings(unittest.TestCase):
+    def test_default_mapping(self):
+        mapping = builtin_mappings['default']
+        path = '/path/to/package/tests/test_something.py'
+
+        self.assertEqual('/path/to', mapping.map('/path/to/module.py'))
+        self.assertEqual('/path/to/nonpython', mapping.map('/path/to/nonpython'))
+        self.assertEqual(path, mapping.map(path))
+
+    def test_external_mapping(self):
+        mapping = builtin_mappings['external']
+        variables = {'BASEPATH': '/path/to/project', 'TESTDIR': 'tests'}
+
+        self.assertEqual(
+            '/path/to/project/tests/test_module.py',
+            mapping.map('/path/to/project/module.py', variables)
+        )
+        self.assertEqual(
+            '/path/to/project/tests/package/test_module.py',
+            mapping.map('/path/to/project/package/module.py', variables)
+        )
+        self.assertEqual('/path/to/nonpython', mapping.map('/path/to/nonpython', variables))
+        self.assertEqual(
+            '/path/to/project/tests/test_something.py',
+            mapping.map('/path/to/project/tests/test_something.py', variables)
+        )
